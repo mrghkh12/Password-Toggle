be26f75a3d1e0ab611544544b249693fa866a582
mohammad reza <mrghkh12@gmail.com>
2024-07-21 15:41:10 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-const $ = document
+const $ = document;
 
-let passwordElem = $.getElementById('passwordElem')
-let btnIcon = $.getElementById('btnIcon')
+let passwordElem = $.getElementById("passwordElem");
+let btnIcon = $.getElementById("btnIcon");
 
-btnIcon.addEventListener('click' , () => {
-    if(passwordElem.type == 'password'){
-        passwordElem.type = 'text'
-        btnIcon.src = './image/eye-open.png'
-    }else{
-        passwordElem.type = 'password'
-        btnIcon.src = './image/eye-close.png'
-    }
-})
+btnIcon.addEventListener("click", () => {
+  if (passwordElem.type == "password") {
+    passwordElem.type = "text";
+    btnIcon.src = "./image/eye-open.png";
+  } else {
+    passwordElem.type = "password";
+    btnIcon.src = "./image/eye-close.png";
+  }
+});
